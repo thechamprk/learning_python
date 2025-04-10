4cfda65d42b9372fe891097717f17b61ff7f8e6f
thechamprk <rounak.k.mahto@gmail.com>
2025-04-10 17:48:30 +0530

    Day 1
    I'll post my work regularly. 🔥

---
+++ input.py
@@ -0,0 +1,8 @@
+#Task: Ask two questions: Person's Name and favouriate color. Then, print a message like "Mosh likes blue"
+
+#My answer
+person_name = input("What's your name? ")
+print('Hi ' + person_name + '!')
+favouriate_color = input("What's your favouriate color? ")
+
+print(person_name + ' likes ' + favouriate_color)
---
+++ task_3.py
@@ -0,0 +1,6 @@
+#Ask a user their weight (in pounds), convert it to kilograms and print on the terminal.
+
+#My Solution
+weight_lbs = input("Your weight(lbs) : ")
+weight_kg = float(weight_lbs) * 0.456592
+print(weight_kg, 'kg')
